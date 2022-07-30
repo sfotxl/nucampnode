e7cdc82ebaf75b05db0bd9bac0d9ac6ad1df7f78
Kalani Man <kromelo@icloud.com>
2022-07-30 10:15:07 -0500

    Putting it all together

---
+++ routes/promotionRouter.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const promotionRouter = express.Router();
+
+promotionRouter
+  .route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  .get((req, res) => {
+    res.end('Will send all the promotions to you');
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the promotion ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on /promotions');
+  })
+  .delete((req, res) => {
+    res.end('Deleting all promotions');
+  });
+
+promotionRouter
+  .route('/:promotionId')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  .get((req, res) => {
+    res.end(
+      `Will send all details for promotion:${req.params.promotionId} to you`
+    );
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the promotion: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `Put operation not supported on /promotions/${req.params.promotionId}`
+    );
+  })
+  .delete((req, res) => {
+    res.end('Deleting all promotions');
+  });
+
+module.exports = promotionRouter;
